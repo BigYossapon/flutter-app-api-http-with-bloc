996b148a6e77628b70ebb2ef426fc927780553aa
BigYossapon <bigyossapon.forwork@gmail.com>
2023-03-06 11:40:35 +0700

    Update employeedataadd_event.dart

--- lib/src/blocs/api/employees_data_bloc/post/employeedataadd_event.dart
+++ lib/src/blocs/api/employees_data_bloc/post/employeedataadd_event.dart
@@ -2,7 +2,9 @@ part of 'employeedataadd_bloc.dart';
 
 abstract class EmployeedataaddEvent extends Equatable {
   const EmployeedataaddEvent();
+}
 
+class AddEmployeedataEvent extends EmployeedataaddEvent {
   @override
   List<Object> get props => [];
 }
